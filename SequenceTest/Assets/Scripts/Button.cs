690200233720fe58ec22817938490288ab4ef771
KNOWN-UNIVERSE\tylosbor <tylosbor@uat.edu>
2018-03-23 13:22:30 -0700

    First Push

---
+++ SequenceTest/Assets/Scripts/Button.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Button : MonoBehaviour {
+
+	public enum Color {Red, Green, Blue, Yellow, Orange, Purple };
+	public Color buttonColor;
+	[HideInInspector]
+	public bool wasPressed = false;
+	public int buttonIndex;
+	public event Action<Button> ButtonPressed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		OnPressed();
+	}
+
+	public void OnPressed()
+	{
+		if(buttonColor == Color.Red)
+		{
+			if (Input.GetKeyDown(KeyCode.R))
+			{
+				wasPressed = true;
+				Debug.Log("You pressed the " + buttonColor.ToString() + "\nIndex: " + buttonIndex);
+				if(ButtonPressed != null)
+				{
+					ButtonPressed(this);
+				}
+			}
+		}
+
+		if (buttonColor == Color.Blue)
+		{
+			if (Input.GetKeyDown(KeyCode.B))
+			{
+				wasPressed = true;
+				Debug.Log("You pressed the " + buttonColor.ToString() + "\nIndex: " + buttonIndex);
+				if (ButtonPressed != null)
+				{
+					ButtonPressed(this);
+				}
+			}
+		}
+
+		if (buttonColor == Color.Green)
+		{
+			if (Input.GetKeyDown(KeyCode.G))
+			{
+				wasPressed = true;
+				Debug.Log("You pressed the " + buttonColor.ToString() + "\nIndex: " + buttonIndex);
+				if (ButtonPressed != null)
+				{
+					ButtonPressed(this);
+				}
+			}
+		}
+
+		if (buttonColor == Color.Orange)
+		{
+			if (Input.GetKeyDown(KeyCode.O))
+			{
+				wasPressed = true;
+				Debug.Log("You pressed the " + buttonColor.ToString() + "\nIndex: " + buttonIndex);
+				if (ButtonPressed != null)
+				{
+					ButtonPressed(this);
+				}
+			}
+		}
+
+		if (buttonColor == Color.Yellow)
+		{
+			if (Input.GetKeyDown(KeyCode.Y))
+			{
+				wasPressed = true;
+				Debug.Log("You pressed the " + buttonColor.ToString() + "\nIndex: " + buttonIndex);
+				if (ButtonPressed != null)
+				{
+					ButtonPressed(this);
+				}
+			}
+		}
+
+		if (buttonColor == Color.Purple)
+		{
+			if (Input.GetKeyDown(KeyCode.P))
+			{
+				wasPressed = true;
+				Debug.Log("You pressed the " + buttonColor.ToString() + "\nIndex: " + buttonIndex);
+				if (ButtonPressed != null)
+				{
+					ButtonPressed(this);
+				}
+			}
+		}
+
+		wasPressed = false;
+	}
+}
---
+++ SequenceTest/Assets/Scripts/SequenceManager.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceManager : MonoBehaviour {
+
+	public List<Button> masterSequence;
+	public Queue<Button> pressedButtons;
+	public int[] sequenceSizes;
+	int currentSequence = 0;
+	int currentSequenceSize;
+	[HideInInspector]
+	public Console[] consoles;
+
+	// Use this for initialization
+	void Start () {
+
+		consoles = FindObjectsOfType<Console>();
+		CreateSequence();
+		currentSequenceSize = sequenceSizes[currentSequence];
+		pressedButtons = new Queue<Button>(currentSequenceSize);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Authenticate();
+	}
+
+	public void CreateSequence()
+	{
+		foreach(Console console in consoles)
+		{
+			foreach(Button button in console.buttons)
+			{
+				masterSequence.Add(button);
+				button.ButtonPressed += AddPressedButtonToList;
+			}
+
+		}
+
+		ShuffleSequence();
+	}
+
+	void ShuffleSequence()
+	{
+		for (int count = 0; count < masterSequence.Count; count++)
+		{
+			int randomValue = UnityEngine.Random.Range(count, masterSequence.Count);
+			Button holder = masterSequence[randomValue];
+			masterSequence[randomValue] = masterSequence[count];
+			masterSequence[count] = holder;
+		}
+	}
+
+	void AddPressedButtonToList(Button _button)
+	{
+		
+		if (pressedButtons.Count + 1 > currentSequenceSize)
+		{
+			
+			pressedButtons.Dequeue ();
+			pressedButtons.Enqueue (_button); 
+		}
+		else
+		{
+			pressedButtons.Enqueue(_button);
+		}
+
+	}
+
+	void Authenticate()
+	{
+		bool authenticated = false;
+
+		if (pressedButtons.Count == currentSequenceSize)
+		{
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				Button[] listToCheck = pressedButtons.ToArray();
+				int offset = 0;
+				for (int count = 0; count < currentSequence; count++)
+				{
+					offset += sequenceSizes [count];
+
+				}
+
+				
+				for (int count = offset; count < currentSequenceSize + offset; count++) {
+					if (listToCheck [count - offset].buttonIndex == masterSequence [count].buttonIndex) {
+						authenticated = true;
+					}
+					else
+					{
+						authenticated = false;
+						break;
+					}
+				}
+
+				if (authenticated)
+				{
+					pressedButtons.Clear();
+					if (currentSequence < sequenceSizes.Length - 1) {
+						currentSequence++;
+						currentSequenceSize = sequenceSizes[currentSequence];
+					}
+					Debug.Log("Authenticated");
+					
+
+				}
+				else
+				{
+					pressedButtons.Clear ();
+					Debug.Log("Rejected");
+				}
+			}
+		}
+	}
+}
